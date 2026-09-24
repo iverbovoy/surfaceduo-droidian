cd2f4984f44b54aef0f5ccfcf5fd6a19ec15b8ba
Ivan Verbovoy <ivanverbovoy@gmail.com>
2026-09-24 20:38:02 +0300

    package: 0.18.0

--- adaptation/package/build.sh
+++ adaptation/package/build.sh
@@ -11,7 +11,7 @@ ACCESS="$HERE/../access"
 SYSTEM="$HERE/../system"
 SHELLDIR="$HERE/../shell"
 BUSYBOX="$ROOT/out/busybox-arm64"
-VER="${1:-0.17.0}"
+VER="${1:-0.18.0}"
 OUT="$ROOT/out"
 PKG="$OUT/pkgroot"
 
